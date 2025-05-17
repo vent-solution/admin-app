4b74d1ab86f910f29493986471da5123a7d1cede
Anatoli <you@example.com>
2025-05-17 11:50:20 +0300

    Anatoli

--- src/index.tsx
+++ src/index.tsx
@@ -35,13 +35,13 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router basename="/admin">
-        <App />
-      </Router>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <Router basename="/admin">
+      <App />
+    </Router>
+  </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
